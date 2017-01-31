610fdae56270d930d8ed9a71e8120e4ed1564069
HENNY <guerrahenny@gmail.com>
2017-01-31 15:11:30 -0800

    First commit

---
+++ lib/circle.rb
@@ -0,0 +1,20 @@
+class Circle
+	attr_reader :x, :y, :radius
+
+	def initialize(radius, x=0, y=0)
+		@radius = radius
+		@x, @y = x, y
+	end
+
+	def diameter
+		@radius * 2
+	end
+
+	def area
+    Math::PI * (@radius ** 2)
+  end
+
+  def perimeter
+    2 * Math::PI * @radius
+  end
+end
---
+++ lib/square.rb
@@ -0,0 +1,38 @@
+class Square
+	attr_reader :x, :y, :side, :length, :width
+
+	def initialize(side, x=0, y=0)
+		@side = side 
+		@x, @y = x, y
+		@width = @side
+		@length = @side
+	end
+
+	def diameter
+    Math.sqrt((@length.to_f ** 2) + (@width.to_f ** 2))
+	end
+
+	def perimeter
+		@width * 2 + @length * 2
+	end
+
+	def area
+		@side * @side
+	end
+
+	def contains_point?(x_1, y_1)
+		right_edge = @x + (@width / 2.0)
+		left_edge = @x - (@width / 2.0)
+		top_edge = @y + (@length / 2.0)
+		bottom_edge = @y - (@length / 2.0)
+
+		if (x_1 <= right_edge && x_1 >= left_edge) && (y_1 <= top_edge && y_1 >= bottom_edge)
+			true
+		else
+			false
+		end
+		
+	end
+end
+
+
